6ca1f3a6201180a04f6c6e4fc79a9bfaccf4d8a8
sandywyper <sandywyper@gmail.com>
2020-10-06 20:06:45 +0100

    Fix title sharing image

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#f2f7f4`,
         theme_color: `#aec0d8`,
         display: `minimal-ui`,
-        icon: `./static/TWS-title-image.jpg` // This path is relative to the root of the site.
+        icon: `./static/cube-icon-black.png` // This path is relative to the root of the site.
       }
     }
   ]
