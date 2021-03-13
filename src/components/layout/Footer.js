0e8100cb88aac555c8f9d6ee3e8a2404106f2c1b
sandywyper <sandywyper@gmail.com>
2021-03-13 13:34:39 +0000

    STYLE - amend footer padding

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,7 @@ import MailIcon from '../../svgComponents/SvgMail.js';
 const Footer = () => (
   <footer className="flex-grow bg-theme-black text-theme-white">
     <div className="container flex flex-col py-10 mx-auto lg:py-16 md:flex-row">
-      <div className="w-full px-3 mb-6 md:1/2 lg:w-1/3">
+      <div className="w-full px-4 mb-6 md:1/2 lg:w-1/3">
         {/* <h2 className="text-lg font-semibold">About TWS</h2> */}
         {/* <TwsTitle classes="fill-current width-15rem h-8" /> */}
         <p className="text-xl font-semibold font-display">Tinderbox Web Solutions</p>
